93a463a997412b076f0ba0ae4c2073d75365bfd5
Flominatortm <flominator@gmx.net>
2021-03-26 18:36:05 +0100

    removing orphans and people without year

--- ged2json.py
+++ ged2json.py
@@ -14,6 +14,7 @@ parser.add_argument('gedcom', type=str, help='path to GEDCOM file')
 parser.add_argument('json', type=str, help='path to JSON file')
 args = parser.parse_args()
 
+nodesWithFamily = []
 res = {}
 res["nodes"] = []
 res["links"] = []
@@ -27,7 +28,10 @@ with open(args.gedcom, encoding='utf8') as f:
         line = lines[i]
         if " INDI" in line:
             if "id" in node:
-                res["nodes"].append(node)
+                if "value" in node:
+                    res["nodes"].append(node)
+                else:
+                    print("skipping yearless " + node["id"] + ": " + node["name"])
             lineParts = line.split()
             node = {"id" : lineParts[1]}
         elif "1 NAME" in line:
@@ -39,15 +43,27 @@ with open(args.gedcom, encoding='utf8') as f:
                 node["value"] =  match.group(1)
         elif "1 HUSB " in line:
             husb = line[7:].strip()
-            print ("husb" + husb)
         elif "1 WIFE " in line:
             wife = line[7:].strip()
         elif "1 CHIL " in line:
+            nodesWithFamily.append(husb)
+            nodesWithFamily.append(wife)
             link = {"source" : husb, "target" : line[7:].strip() , "directed": True}
             res["links"].append(link)
             link = {"source" : wife, "target" : line[7:].strip(), "directed": True}
             res["links"].append(link)
-        print (node)
+
+nodesWithoutParents = []
+for oneNode in res["nodes"]:
+    if oneNode["id"] not in nodesWithFamily:
+        nodesWithoutParents.append(oneNode)
+    else:
+        print("has family " + oneNode["id"] + ": " + oneNode["name"])
+        
+
+for orphanNode in nodesWithoutParents:
+    print ("removing orphan " + orphanNode["id"] + ": " + orphanNode["name"])
+    res["nodes"].remove(orphanNode)
 
 with open(args.json, 'w', encoding="utf8") as outfile:
     json.dump(res, outfile, indent=3)
